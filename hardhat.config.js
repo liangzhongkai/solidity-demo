2704f463cbc0f488e5300aae1fa3e37ba2fe89d8
lzhkai <lzhkai1111@qq.com>
2026-02-02 12:43:29 +0800

    migrate from win11 to linux

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,7 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: {
     version: "0.8.20",
+    solcjs: "solc",
     settings: {
       optimizer: {
         enabled: true,
